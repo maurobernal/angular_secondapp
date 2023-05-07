a46c68df5eabab1663f165f4ea86f154c31d4cd2
Mauro Bernal <info@maurobernal.com.ar>
2023-05-07 16:24:57 -0300

    feat:MB:Second Lesson

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from 'mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero: Hero | undefined ;
+
+  onSelectHero(hero: Hero) : void{
+    this.selectedHero= hero;
+  }
+
+}
