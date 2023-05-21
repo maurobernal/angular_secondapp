4f1d271a737f888ee8b5cd766c3d69c7a1f0d737
Mauro Bernal <info@maurobernal.com.ar>
2023-05-21 15:21:49 -0300

    feat:MB: Add ServiceHeroes and Component HeroeDetails
    Using rxjs for Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from 'mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
   selectedHero: Hero | undefined ;
+
+  constructor(private heroService: HeroService){}
+  
+  ngOnInit(): void {
+    this.getHeroesObservable();
+  }
+
 
   onSelectHero(hero: Hero) : void{
     this.selectedHero= hero;
+  }
+
+  getHeroes():void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  getHeroesObservable():void {
+    this.heroService.getHeroes$().subscribe( (resp: Hero[]) => this.heroes = resp);
   }
 
 }
